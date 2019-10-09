ba76ad7443998d0473637e51be62ce9076e61d8f
Marina Sheenkova <marina@astrails.com>
2019-10-09 21:19:59 +0300

    Updates

---
+++ precache-manifest.6b6cfef0de6f424c8c052de1c46d654d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120cbf1bdd753e164bc2be7c5abe7d65",
+    "url": "/rain_wall/index.html"
+  },
+  {
+    "revision": "1f40652df0cd9bc03a15",
+    "url": "/rain_wall/static/css/main.166ed8c4.chunk.css"
+  },
+  {
+    "revision": "f9026ee7de81d3248ccb",
+    "url": "/rain_wall/static/js/2.2560bd29.chunk.js"
+  },
+  {
+    "revision": "1f40652df0cd9bc03a15",
+    "url": "/rain_wall/static/js/main.85c2313b.chunk.js"
+  },
+  {
+    "revision": "ff5b314241765a792b00",
+    "url": "/rain_wall/static/js/runtime-main.9615a87e.js"
+  },
+  {
+    "revision": "4a1191680ffae876331856b04a550ce3",
+    "url": "/rain_wall/static/media/clouds.4a119168.png"
+  },
+  {
+    "revision": "c6f0214ca7fbce5bdb857acf9369c07c",
+    "url": "/rain_wall/static/media/grass.c6f0214c.png"
+  }
+]);
